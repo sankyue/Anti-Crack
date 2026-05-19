9ff6e53a456551a242c6d0d00ee34929149c132a
sankyue <usukhbayar4581@gmail.com>
2026-05-19 08:37:13 +0800

    Add GetExecutablePath function to integrity.h
    Added GetExecutablePath function for retrieving the executable path.

--- AntiCrack/AntiCrack/include/integrity.h
+++ AntiCrack/AntiCrack/include/integrity.h
@@ -1,17 +1,13 @@
 #pragma once
-#include <string>
 #include <cstdint>
+#include <string>
 #include <vector>
 
 namespace Integrity {
-
-    // CRC32 hash тооцох
     uint32_t ComputeCRC32(const std::string& filepath);
-
-    // Файлын hash-ийг хадгалсантай харьцуулах
     bool VerifyFileHash(const std::string& filepath, uint32_t expectedCRC);
+    std::string GetExecutablePath();
 
-    // Өөрийн exe-г шалгах (self-integrity)
     bool VerifySelf();
 
-} // namespace Integrity
+} 
